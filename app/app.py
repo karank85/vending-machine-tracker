56d2a7eaf34d742f072a1d8eb33dfcac21a4c09b
karank85 <kkjas85@gmail.com>
2023-01-14 22:01:36 +0700

    init commit

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mysqldb import MySQL
+import yaml
+
+app = Flask(__name__)
+
+cred = yaml.load(open('../cred.yaml'), Loader=yaml.Loader)
+app.config['MYSQL_HOST'] = cred['mysql_host']
+app.config['MYSQL_USER'] = cred['mysql_user']
+app.config['MYSQL_PASSWORD'] = cred['mysql_password']
+app.config['MYSQL_DB'] = cred['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+@app.route('/', endpoint="home")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
