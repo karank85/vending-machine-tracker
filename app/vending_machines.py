5642eb9f30c491b05ef6d55e5f29227849366dbb
karank85 <kkjas85@gmail.com>
2023-01-16 01:26:47 +0700

    implemented api to get one vending machine by id

--- app/vending_machines.py
+++ app/vending_machines.py
@@ -21,7 +21,19 @@ def all_vending_machines():
 
 @vending_bp.route("/vending-machine", methods=['GET'])
 def vending_machine():
-    pass
+
+    from app import mysql
+
+    id = request.form['id']
+    
+    cur = mysql.connection.cursor()
+    query_statement = f"SELECT * FROM vending_machine WHERE vending_machine_id={id}"
+    output_rows = cur.execute(query_statement)
+    
+    if output_rows > 0:
+        vm = cur.fetchone()
+        return jsonify(vm)
+    return None
 
 @vending_bp.route('/vending-machine/delete')
 def delete_vending_machine():
