f6738141f442b6f07d840b791069b0a8a39eeb3b
karank85 <kkjas85@gmail.com>
2023-01-16 21:46:02 +0700

    implemented api to edit listing

--- app/listing.py
+++ app/listing.py
@@ -75,5 +75,19 @@ def create_listing():
 
 @listing_bp.route('/listing/edit', methods=['POST'])
 def edit_listing():
-    pass
+    
+    from app import mysql
 
+    id = request.form['id']
+    vending_machine_id = request.form['vending_machine_id']
+    product_id = request.form['product_id']
+    quantity = request.form['quantity']
+
+    cur = mysql.connection.cursor()
+    query_statement = f"UPDATE listing SET product_id = {product_id}, vending_machine_id = {vending_machine_id}, quantity = {quantity} WHERE listing_id={id}"
+    output = cur.execute(query_statement)
+
+    if output > 0:
+        cur.close()
+        return listing()
+    return jsonify(None)
